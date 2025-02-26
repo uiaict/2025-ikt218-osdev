8f8b42c93c86b9c09b43e0a984159e14c76ad8fb
Aryan Aziz Mathiesen <aryanam@uia.no>
2025-02-26 13:08:49 +0000

    Implemented strlen & print hello world to screen

--- src/2_mangOS/src/kernel.c
+++ src/2_mangOS/src/kernel.c
@@ -1,6 +1,7 @@
 #include "libc/stdint.h"
 #include "libc/stddef.h"
 #include "libc/stdbool.h"
+#include "libc/string.h"
 #include <multiboot2.h>
 
 struct multiboot_info
@@ -12,5 +13,18 @@ struct multiboot_info
 
 int main(uint32_t magic, struct multiboot_info *mb_info_addr)
 {
+
+    char *hello_world = "Hello, World!";
+    size_t len = strlen(hello_world);
+
+    // Write to video memory
+    char *video_memory = (char *)0xb8000;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        video_memory[i * 2] = hello_world[i];
+        video_memory[i * 2 + 1] = 0x07;
+    }
+
     return 0;
 }
