3fb0643e99af7b2aebe4d1fdf4fd7289ec462b98
Magnus Johannessen <magnusjoh03@gmail.com>
2025-03-04 16:36:47 +0100

    Added a GDT

--- src/3_OSDev/src/kernel.c
+++ src/3_OSDev/src/kernel.c
@@ -1,9 +1,15 @@
 #include "libc/stdint.h"
 #include "libc/stddef.h"
 #include "libc/stdbool.h"
+
 #include <multiboot2.h>
 
+#include <gdt.h>
 
+#define VGA_MEMORY 0xB8000
+#define screen_width 80
+int x = 0;
+int y = 0;
 
 struct multiboot_info {
     uint32_t size;
@@ -11,9 +17,27 @@ struct multiboot_info {
     struct multiboot_tag *first;
 };
 
+// Sources: http://www.brokenthorn.com/Resources/OSDev10.html
+//          https://wiki.osdev.org/Printing_To_Screen
+void terminal_write( int colour, const char *string ) {
+    int memory_location;                                                // Declares an integer var   
+    memory_location = VGA_MEMORY + (x + (y * screen_width) * 2);        // Calculates the memory location (based of source) 
+                                                                            // Multiplication by 2 is because each character is 2 bytes
+    volatile char *video = (volatile char*)memory_location;             // Declares a volatile char pointer and assigns the memory location to it
+    while (*string != 0) {                                              
+        *video++ = *string++;                                           // Writes the string to the video memory
+        *video++ = colour;                                              // Writes the colour to the video memory              
+    }   
+    y++;                                                                // Increments the y value           
+}
 
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
 
+    // Initialize GDT
+    init_gdt();
+    
+    // Print "Hello World!" to screen
+    terminal_write(0x0F, "Hello World!");
     return 0;
 
 }
