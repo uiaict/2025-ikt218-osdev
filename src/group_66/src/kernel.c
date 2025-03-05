1c49f91fb1b4d851ac24f66c39d276928b802930
Aris <99477476+PinOstrich@users.noreply.github.com>
2025-03-05 14:45:08 +0000

    Implemented GDT and basic printf function to print text

--- src/group_66/src/kernel.c
+++ src/group_66/src/kernel.c
@@ -2,7 +2,18 @@
 #include "libc/stddef.h"
 #include "libc/stdbool.h"
 #include <multiboot2.h>
+#include "gdt.h"
 
+
+#define VGA_ADDRESS 0xB8000
+#define VGA_ROWS 25
+#define VGA_COLUMS 80
+
+int cursor_x = 0;
+int cursor_y = 0;
+
+uint16_t vga_mem_index = 0;
+volatile char * vga_buffer = (volatile char*)VGA_ADDRESS;
 
 
 struct multiboot_info {
@@ -11,9 +22,24 @@ struct multiboot_info {
     struct multiboot_tag *first;
 };
 
+void printf(int color, const char* str) {
+    
+    while(*str != '\0') {
+        if(cursor_x >=80 || *str == '\n') {
+            cursor_x = 0;
+            cursor_y++;
+        }
+        int index = (cursor_y * 80 + cursor_x) * 2;
+        vga_buffer[index] = *str; 
+        vga_buffer[index+1] = color; 
+        str++;
+        cursor_x++;
+    }
+}
 
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
-
+    initGdt();
+    printf(15, "Hello, world!");
     return 0;
 
 }
