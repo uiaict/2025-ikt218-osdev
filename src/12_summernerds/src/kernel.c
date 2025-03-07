51835c589bd4a26a55bc69653807f9edacd65bab
Sindre Thomas Lorentsen <sindretl@uia.no>
2025-03-07 14:38:12 +0000

    finished kernel.c file

--- src/12_summernerds/src/kernel.c
+++ src/12_summernerds/src/kernel.c
@@ -3,19 +3,18 @@
 #include "libc/stdbool.h"
 #include <multiboot2.h>
 
+#define VGA_WIDTH  80
+#define VGA_HEIGHT 25
+#define VGA_MEMORY (volatile uint16_t*)0xB8000
 
+uint8_t summer_colours = {0x4, 0xE, 0x2, 0x9}; 
+struct multiboot_info {uint32_t size; uint32_t reserved; struct multiboot_tag *first;};
 
-struct multiboot_info {
-    uint32_t size;
-    uint32_t reserved;
-    struct multiboot_tag *first;
-};
-
-
-int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
-
-    
-
-    return 0;
-
+void add_colours_when_write_to_terminal(const char* str) {
+    volatile uint16_t* vga = VGA_MEMORY;
+    for (int i = 0; str[i]; i++)
+        vga[i] = (rainbow_colors[i % 4] << 8) | str[i];
 }
+
+int main(uint32_t magic, struct multiboot_info* mb_info_addr)
+{add_colours_when_write_to_terminal("Hello Summernerds!!!")return 0;}
