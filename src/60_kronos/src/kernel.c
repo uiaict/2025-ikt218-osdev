68c3b28558c1e67bd18b7fdce430231276f0386c
Jakob Moen Bekken <jakobmb@uia.no>
2025-03-09 18:26:55 +0100

    Init with GDT

---
+++ src/60_kronos/src/kernel.c
@@ -0,0 +1,25 @@
+#include "libc/stdint.h"
+#include "libc/stddef.h"
+#include "libc/stdbool.h"
+#include "libc/stdio.h"
+#include <multiboot2.h>
+
+#include "gdt/gdt.h"
+
+
+
+struct multiboot_info {
+    uint32_t size;
+    uint32_t reserved;
+    struct multiboot_tag *first;
+};
+
+
+int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
+
+    gdt_init();
+    // TODO: printf("GDT initialized\n\n");
+
+    return 0;
+
+}
