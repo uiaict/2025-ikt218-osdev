9464098b1990f6db04961821c51ab35eb3932fe0
Sverre <sverrir93@hotmail.com>
2025-04-02 11:31:31 +0000

    Initial Commit

--- src/SverreNils/src/kernel.c
+++ src/SverreNils/src/kernel.c
@@ -1,3 +1,5 @@
+#include "arch/gdt.h"
+#include "printf.h"
 #include "libc/stdint.h"
 #include "libc/stddef.h"
 #include "libc/stdbool.h"
@@ -11,9 +13,16 @@ struct multiboot_info {
     struct multiboot_tag *first;
 };
 
+void putc_raw(char c) {
+    volatile char* video = (volatile char*)0xB8000;
+    video[0] = c;
+    video[1] = 0x07;
+}
 
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
-
+    putc_raw('Z');
+    gdt_init();
+    printf("Hello, Nils!\n");
     return 0;
 
 }
