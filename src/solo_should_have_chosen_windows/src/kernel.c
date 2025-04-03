2a42126294cea86108110005ad9324a3d482cf35
Adam Hazel <adamrhazel@gmail.com>
2025-04-03 10:17:05 +0200

    Added end variable

--- src/solo_should_have_chosen_windows/src/kernel.c
+++ src/solo_should_have_chosen_windows/src/kernel.c
@@ -8,7 +8,7 @@
 
 #include <multiboot2.h>
 
-
+extern uint32_t end;
 
 struct multiboot_info {
     uint32_t size;
@@ -24,9 +24,11 @@ int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
 
     idt_init();
     printf("idt now loaded\n");
-    
+
     enable_interrupts();
     printf("interrupts enabled\n");
+
+    printf("Kernel ends at: %x\n", &end);
     
        // Keep the CPU running forever
     while (1) {
