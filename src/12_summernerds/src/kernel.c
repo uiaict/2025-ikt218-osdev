81e84aa60b1497004c8ae1dbfd3f1bb8e7a7efcc
Sindre Thomas Lorentsen <sindretl@uia.no>
2025-04-09 13:01:21 +0000

    worked on pit files, kernel.c and added paging and kheap files

--- src/12_summernerds/src/kernel.c
+++ src/12_summernerds/src/kernel.c
@@ -1,19 +1,54 @@
-#include "libc/stdint.h"
-#include "libc/stddef.h"
-#include "libc/stdbool.h"
-#include <multiboot2.h>
+#include <libc/stdint.h>
+#include <libc/stddef.h>
+#include <libc/stdbool.h>
 #include "screen.h"
-#include "Keyboard.c"
+#include "keyboard.h"
+#include "kheap.h"         // for malloc(), free(), init_kernel_memory()
+#include "paging.h"        // for init_paging()
+#include "pit.h"           // for init_pit(), sleep functions
 
-struct multiboot_info {
-    uint32_t size; 
-    uint32_t reserved; 
-    struct multiboot_tag *first;
-};
+extern uint32_t end;       // Linker symbol marking the end of kernel
 
-int main(uint32_t magic, struct multiboot_info* mb_info_addr)
-{
-    write_to_terminal("Hello Summernerds!!!", 1);
+int kernel_main_c(uint32_t magic, uint32_t mb_info_addr) {
 
+    //initializing basic systems
+    monitor_initialize();
+    init_gdt();
+    init_idt();
+    init_irq();
+
+    // Initializing the kernel memory manager
+    init_kernel_memory(&end);
+
+    // call function to activate paging
+    init_paging();
+
+    // primt memory layout to screen
+    print_memory_layout();
+
+    // Initialize PIT (programable interval timer)
+    init_pit();
+
+    // Here we test the memory allocation
+    void* mem1 = malloc(12345);
+    void* mem2 = malloc(54321);
+    void* mem3 = malloc(13331);
+
+    // print if we get any problem with allocating memory
+    printf("Allocated memory blocks at: %p, %p, %p\n", mem1, mem2, mem3);
+
+    // Test PIT sleep
+    int counter = 0;
+    while (true) {
+        printf("[%d]: Sleeping with busy-waiting (HIGH CPU)...\n", counter);
+        sleep_busy(1000);
+        printf("[%d]: Slept using busy-waiting.\n", counter++);
+
+        printf("[%d]: Sleeping with interrupts (LOW CPU)...\n", counter);
+        sleep_interrupt(1000);
+        printf("[%d]: Slept using interrupts.\n", counter++);
+    }
+
+    // Usually shouldnt get here, since it then quits kernel main.
     return 0;
 }
