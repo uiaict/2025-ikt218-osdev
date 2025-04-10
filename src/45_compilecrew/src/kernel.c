4d9f97b8924a1c701a450fe0ca65556f03ac5336
Joakim Bratland <joakimbr@uia.no>
2025-04-10 13:08:26 +0000

    added idt files

--- src/45_compilecrew/src/kernel.c
+++ src/45_compilecrew/src/kernel.c
@@ -6,6 +6,8 @@
 #include "libc/gdt.h"
 #include "libc/terminal.h"
 #include "libc/printf.h"
+#include "libc/idt.h"
+
 
 struct multiboot_info {
     uint32_t size;
@@ -13,14 +15,21 @@ struct multiboot_info {
     struct multiboot_tag *first;
 };
 
-int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
+__attribute__((noreturn)) void exception_handler() {
+    __asm__ volatile("cli; hlt");
+    __builtin_unreachable();
+}
 
+
+int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
+    idt_init();
     int noop = 0;
     init_gdt();
     
     printf("Hello World\n");
     printf("Number: %d\n", 42);
     printf("Hex: %x\n", 255);
+    int x = 1 / 0;
     printf("Character: %c\n", 'A');
     printf("String: %s\n", "Kernel Booted");
     
