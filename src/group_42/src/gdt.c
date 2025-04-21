d767e34d34eb15b1c2d8317dc2c8631c02f72551
Cyrus Harding <10121762+Cyrus-Harding@users.noreply.github.com>
2025-04-21 19:25:48 +0200

    Add credits to files

--- src/group_42/src/gdt.c
+++ src/group_42/src/gdt.c
@@ -1,3 +1,5 @@
+// Most of this file is taken from Bran's Kernel Development Tutorial
+// http://www.osdever.net/bkerndev/Docs/gdt.htm
 
 /* Defines a GDT entry. We say packed, because it prevents the
  *  compiler from doing things that it thinks is best: Prevent
