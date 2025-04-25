24f33ebada44c2acfcf3a838a02da345781a6605
AndreasOtterstad <166396848+AndreasOtterstad@users.noreply.github.com>
2025-04-25 13:48:03 +0200

    Update kernel.cpp

--- src/69_MutexMafia/src/kernel.cpp
+++ src/69_MutexMafia/src/kernel.cpp
@@ -5,10 +5,22 @@ extern "C" {
     #include "idt/idt.h"
     #include "io/keyboard.h"
     #include "io/printf.h"
-
+    #include "pit/pit.h"
 
     int kernel_main(){
         mafiaPrint("Kernel main function started\n");
+        init_pit(); 
+        /*
+        while(true){
+            int counter = 0;
+            mafiaPrint("[%d]: Sleeping with busy-waiting (HIGH CPU).\n", counter);
+            sleep_busy(1000);
+            mafiaPrint("[%d]: Slept using busy-waiting.\n", counter++);
+    
+            mafiaPrint("[%d]: Sleeping with interrupts (LOW CPU).\n", counter);
+            sleep_interrupt(1000);
+            mafiaPrint("[%d]: Slept using interrupts.\n", counter++);
+        }; */
         while (1) {}
     }
 }
