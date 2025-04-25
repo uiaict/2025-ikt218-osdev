84a8957d269359045e0220a55fcd3a3b5948756f
nils jakobsen <nilsaj@uia.no>
2025-04-25 07:01:58 +0000

    lagg in en shutdown kommando

--- src/SverreNils/src/shell.c
+++ src/SverreNils/src/shell.c
@@ -1,4 +1,3 @@
-
 #include "printf.h"
 #include <stdint.h>
 #include <stddef.h>
@@ -9,15 +8,28 @@ void shell_prompt() {
     printf("UiAOS> ");
 }
 
+// Skriver til I/O-port (brukes for QEMU shutdown)
+static inline void outw(uint16_t port, uint16_t val) {
+    __asm__ volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// Kaller QEMU ACPI shutdown
+void shutdown() {
+    outw(0x604, 0x2000);
+}
+
 void shell_handle_input(const char* input) {
     if (strcmp(input, "help") == 0) {
         printf("Tilgjengelige kommandoer:\n");
-        printf(" - help\n - clear\n - echo [tekst]\n");
+        printf(" - help\n - clear\n - echo [tekst]\n - shutdown\n");
     } else if (strncmp(input, "echo ", 5) == 0) {
         printf("%s\n", input + 5);
     } else if (strcmp(input, "clear") == 0) {
         for (int i = 0; i < 80 * 25; i++)
             ((uint16_t*)0xB8000)[i] = 0x0720;
+    } else if (strcmp(input, "shutdown") == 0) {
+        printf("Shutting down...\n");
+        shutdown();
     } else {
         printf("Ukjent kommando: %s\n", input);
     }
