956da47be76015acf5056f334e777ad17d4e72d2
aslaugo <aslaugo@uia.no>
2025-05-05 16:24:08 +0000

    added shift functionality

--- src/18_osdev/src/keyboard/keyboard.c
+++ src/18_osdev/src/keyboard/keyboard.c
@@ -5,10 +5,11 @@
 static char input_buffer[INPUT_BUFFER_SIZE];
 static int buffer_index = 0;
 static bool line_ready = false;
+static bool shift_pressed = false; // Track shift key state
 
 extern bool piano_mode_enabled;
 
-// standard keyboard layout for us keyboards
+// standard keyboard layout for us keyboards (lowercase)
 unsigned char kbdus[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
@@ -18,9 +19,9 @@ unsigned char kbdus[128] =
   't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
     0,			/* 29   - Control */
   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
+ '\'', '`',   0,		/* Left shift (42) */
  '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
+  'm', ',', '.', '/',   0,				/* Right shift (54) */
   '*',
     0,	/* Alt */
   ' ',	/* Space bar */
@@ -47,7 +48,48 @@ unsigned char kbdus[128] =
     0,	/* F11 Key */
     0,	/* F12 Key */
     0,	/* All other keys are undefined */
-};	
+};
+
+// Shifted keyboard layout (uppercase and special characters)
+unsigned char kbdus_shift[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',  /* 9 */
+  '(', ')', '_', '+', '\b',  /* Backspace */
+  '\t',      /* Tab */
+  'Q', 'W', 'E', 'R',  /* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',  /* Enter key */
+    0,      /* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',  /* 39 */
+ '"', '~',   0,    /* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',      /* 49 */
+  'M', '<', '>', '?',   0,        /* Right shift */
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
 
 void read_line(char* buffer) {
   // Wait until a full line is entered (user pressed Enter)
@@ -93,21 +135,36 @@ void keyboard_handler(registers_t regs)
         handle_piano_key(scancode);
         return; // Skip normal keyboard input while in piano mode
     }
-    /* If the top bit of the byte we read from the keyboard is
-    *  set, that means that a key has just been released */
+
+    /* Handle key release (top bit set) */
     if (scancode & 0x80)
     {
-        /* We don't handle key releases */
+        // Check if released key is shift
+        unsigned char released_key = scancode & 0x7F; // Remove the top bit to get the actual key
+        
+        if (released_key == 0x2A || released_key == 0x36) { // Left or right shift
+            shift_pressed = false; // shift was released
+        }
         return;
     }
+    
     else
     {
+        /* Handle key press */
+
+        /* Check for shift keys first */
+        if (scancode == 0x2A || scancode == 0x36) { // Left or right shift
+            shift_pressed = true;
+            return;
+        }
+        
         /* Check for ESC key (scancode 0x01) to stop music */
         if (scancode == 0x01) {
             stop_song_requested = true;  // Set the flag to stop any playing song
             return;
         }
-        /* Check for backspace first*/
+        
+        /* Check for backspace */
         if (scancode == 0x0E) {
             if (buffer_index > 0) {
                 buffer_index--; // remove char from buffer index
@@ -116,12 +173,17 @@ void keyboard_handler(registers_t regs)
             return;
         }
 
-        /* For other keys, get the character */
-        char c = kbdus[scancode];
+        /* For other keys, get the character based on shift state */
+        char c;
+        if (shift_pressed) {
+            c = kbdus_shift[scancode]; // use shift keyboard layout
+        } else {
+            c = kbdus[scancode]; // use normal keyboard
+        }
         
         if (c) {
             // Echo character to screen
-            monitor_put(c,color);
+            monitor_put(c, color);
             
             // Handle special characters
             if (c == '\n') {
