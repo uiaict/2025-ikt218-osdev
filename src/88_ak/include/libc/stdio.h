646a9ad8313a4e5f8f94d06bfbf5768cbe67f337
Aron <144605848+theblackswordsman1@users.noreply.github.com>
2025-05-09 21:12:11 +0200

    Update stdio.h

--- src/88_ak/include/libc/stdio.h
+++ src/88_ak/include/libc/stdio.h
@@ -1,5 +1,11 @@
 #pragma once
+#include "libc/stddef.h"
 
-int putchar(int ic);
+// skriver én karakter til VGA‐buffer
+int putchar(int c);
+
+// skriver en buffer av fixed length
 bool print(const char* data, size_t length);
+
+// standard printf
 int printf(const char* __restrict__ format, ...);
